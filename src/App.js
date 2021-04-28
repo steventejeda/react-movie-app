484b388052aba49ac63ec6cc15acabc83c34d6d9
Steven Tejeda <steventejeda@gmail.com>
2021-04-28 17:59:34 -0400

    Added search bar, implemented search API along with CSS styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Movie from './components/Movie';
 
 
 //API key, **This is not a best practice for storing API_KEYs. For time management purposes of completing this project is why I did this.***
-const API_KEY = 'c89425fda98c3c3b8d5707c051751162';
+const API_KEY = '04c35731a5ee918f014970082a0088b1';
 
 //Movie APIs
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
@@ -13,26 +13,56 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  console.log(process.env)
+  function getMovies(API) { 
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => { 
+    console.log(data);
+    setMovies(data.results);
+  });
+  }
 
   useEffect(() =>  {
-      fetch(FEATURED_API)
-        .then((res) => res.json())
-        .then((data) => { 
-        console.log(data);
-        setMovies(data.results);
-      });
+      getMovies(FEATURED_API)
   }, []);
 
+  
+
+  //SEARCH COMPONENT
+    const handleOnSubmit = (e) => { 
+      e.preventDefault();
+
+      getMovies(SEARCH_API+searchTerm)
+    setSearchTerm('');
+    
+    }
+
+    const handleOnChange = (e) => { 
+      setSearchTerm(e.target.value);
+    }
 
   return (
-    
     <div>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+      <input 
+       className="search"
+       type="text" 
+       placeholder="Search..."
+       value={searchTerm}
+       onChange={handleOnChange}
+       />
+      </form>
+      </header> 
+
+    <div className="movie-container">
       {movies.length > 0 && movies.map((movie) => (
         <Movie key={movie.id} {...movie} />
       ))}
       
+    </div>
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
+const setVoteClass = (vote) => { 
+    if (vote >= 8) { 
+    return 'green'
+} else if (vote >=6 ) { 
+    return "orange";
+} else { 
+    return "red";
+}
+}
+
 const Movie = ({ title, poster_path, overview, vote_average }) => {
 
     return (
@@ -9,8 +19,14 @@ const Movie = ({ title, poster_path, overview, vote_average }) => {
         <img src={IMG_API + poster_path} alt={title} />
         <div className="movie-info">
             <h3>{title}</h3>
-            <span>{vote_average}</span>
+            <span className={`tag ${setVoteClass}(vote_average)`}>{vote_average}</span>
         </div>
+
+        <div className="movie-overview">
+            <h2>Overview:</h2>
+            <p>{overview}</p>
+        </div>
+
         </div>
     )
 }
